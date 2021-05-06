993f32de6b05c29a08daf17810d65555bcb13167
Literally Collective <bluedragon.cstar@outlook.com>
2021-05-05 21:41:17 -0300

    Feat: initial commit

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,30 @@
+export default {
+  cake: {
+    56: '0x5068339C71Dbf01ECEbA53f143C1Fd763E178dF0',
+    97: '',
+  },
+  masterChef: {
+    56: '0x44D8B66b0446429C98479D8ee97f74A3a25cE4BA',
+    97: '',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '',
+  },
+}
